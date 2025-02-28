31d80ec63cf06217f33380b6947851fac0f68663
gabrielrandrade <gabrielrandradesp@hotmail.com>
2025-02-28 09:18:27 -0300

    style: create input styles

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,7 +1,9 @@
 import { TextInput, TextInputProps } from "react-native";
+import styles from "./style";
 
-function Input({ ...rest }: TextInputProps){
-    return <TextInput {...rest}/>
+
+function Input({ ...rest }: TextInputProps) {
+  return <TextInput style={styles.input} {...rest} />;
 }
 
 export default Input;
---
+++ src/components/input/style.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  input: {
+    width: "100%",
+    height: 52,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "000",
+    fontSize: 16,
+    padding: 12,
+  },
+});
+
+export default styles;
