764fb350b79915d3b8d758f637a76e0af711c60b
gabrielrandrade <gabrielrandradesp@hotmail.com>
2025-02-28 10:40:04 -0300

    chore: useState to update name

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,26 @@
 import { StyleSheet, Text, View } from "react-native";
 import Button from "../components/button/index"
 import Input from "../components/input";
+import { useState } from "react";
 
 function Index() {
+  const [name, setName] = useState<String>(""); // Inferindo a tipagem
+  const [name1, setName1] = useState(""); // Tipo  Explicitamente declarado
+  
+
   function handleSubmit() {
     const name = "Gabriel";
     alert(`Olá, ${name}`);
   }
+
+  function onChangeText(text:string){
+    console.log(text);
+    setName(text);
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello, World</Text>
-      <Input label={"Nome"} />
+      <Text style={styles.text}>Olá, {name}</Text>
+      <Input label={"Nome"} onChangeText={(text) => onChangeText(text)}/>
       <Input label={"Idade"} />
       <Button label={"Enviar"} onPress={handleSubmit} activeOpacity={1}/>
       <Button label={"Salvar"} onPress={handleSubmit} />
